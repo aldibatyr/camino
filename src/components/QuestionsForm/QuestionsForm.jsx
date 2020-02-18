576596b6500a36ef2bed2764aa2bde744c047ae9
aldibatyr <aldibatyrbekov@gmail.com>
2020-02-17 16:01:58 -0800

    validation of inputs work, need to enable next

--- src/components/QuestionsForm/QuestionsForm.jsx
+++ src/components/QuestionsForm/QuestionsForm.jsx
@@ -45,7 +45,7 @@ function QuestionsForm() {
   }
 
   const validateEmailFieldInput = (e) => {
-    const reg_ex = /\S+@\S+\.\S+/
+    const reg_ex = /\S+@\S+\.\S+/;
     if (reg_ex.test(e.target.value) !== true) {
       setFormFieldsValid({ ...formFieldsValid, [e.target.name]: false })
     } else {
@@ -53,16 +53,28 @@ function QuestionsForm() {
     }
   }
 
-  const validateSelectFieldInput = (e) => {
-    if (e.target.value !== undefined) {
+  const validateRadioFieldInput = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value })
+    setFormFieldsValid({ ...formFieldsValid, [e.target.name]: true })
+  }
 
+  const validateSelectFieldInput = (e) => {
+    if (e.target.value.length !== 0) {
+      return setFormFieldsValid({ ...formFieldsValid, [e.target.name]: true})
+    } 
+  }
+
+  const activateNextButton = (obj) => {
+    for (let key in obj) {
+      !obj[key] ? setFormValid(false) : setFormValid(true)
     }
+    
   }
 
   return (
     <>
       {(step === 1) ? (
-        <form className='main-form' onSubmit={(e) => handleSubmit(e)}>
+        <form className='main-form' onSubmit={(e) => handleSubmit(e)} onChange={() => activateNextButton(formFieldsValid)}>
           <div className="form-introduction">
             <span className='main-text'>Tell us a little bit about yourself and your business loan needs</span>
             <br />
@@ -90,46 +102,53 @@ function QuestionsForm() {
             <label htmlFor="phone">Mobile Phone Number <span className="required">*</span></label>
             <input type="tel" name="phone" id="phone"
               onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
-              obBlur={(e) => validateNumFieldInput(e)} />
+              onBlur={(e) => validateNumFieldInput(e)} />
           </div>
           <div className="input-section">
             <label htmlFor="ownBusiness">Do you own a business? <span className="required">*</span></label>
             <div className="question-answer">
-              <input type="radio" name="ownBusiness" id="ownBusiness" value={true} 
-              onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })} 
+              <input type="radio" name="ownBusiness" id="ownBusiness" value={true}
+                onChange={(e) => validateRadioFieldInput(e)}
               />
               <span>Yes</span>
             </div>
             <div className="question-answer">
-              <input type="radio" name="ownBusiness" id="ownBusiness" value={false} onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })} />
+              <input type="radio" name="ownBusiness" id="ownBusiness" value={false}
+                onChange={(e) => validateRadioFieldInput(e)} />
               <span>No</span>
             </div>
           </div>
           <div className="input-section">
             <label htmlFor="businessName">Business Name <span className="required">*</span></label>
-            <input type="text" name="businessName" id="businessName" onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })} />
+            <input type="text" name="businessName" id="businessName"
+              onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })} 
+              onBlur={(e) => validateTextFieldInput(e)} />
           </div>
           <div className="input-section">
             <label htmlFor="loanAmount">Desired Loan Amount <span className="required">*</span></label>
-            <input type="number" name="loanAmount" id="loanAmount" onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })} />
+            <input type="number" name="loanAmount" id="loanAmount" 
+              onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+              onBlur={(e) => validateNumFieldInput(e)} />
           </div>
           <div className="input-section">
             <label htmlFor="purposeOfLoan">Purpose of Loan <span className="required">*</span></label>
-            <select name="purposeOfLoan" id="purpose" required onBlur={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}>
+            <select name="purposeOfLoan" id="purpose" 
+              onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })}
+              onBlur={(e) => validateSelectFieldInput(e)}>
               <option value=""></option>
               <option value="personal">Personal</option>
               <option value="growth">Growth</option>
-              <option value="suit">Build Iron-Man suit</option>
+              <option value="suit">Build the Iron-Man suit</option>
             </select>
           </div>
           <div className="input-section">
             <label htmlFor="question">Have you been in business for at least 9 months? <span className="required">*</span></label>
             <div className="question-answer">
-              <input type="radio" name="isBusinessOver9Months" id="question" value={true} onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })} />
+              <input type="radio" name="isBusinessOver9Months" id="question" value={true} onChange={(e) => validateRadioFieldInput(e)} />
               <span>Yes</span>
             </div>
             <div className="question-answer">
-              <input type="radio" name="isBusinessOver9Months" id="question" value={false} onChange={(e) => setFormState({ ...formState, [e.target.name]: e.target.value })} />
+              <input type="radio" name="isBusinessOver9Months" id="question" value={false} onChange={(e) => validateRadioFieldInput(e)} />
               <label htmlFor="question">No</label>
             </div>
           </div>
